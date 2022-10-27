2909bff0ca5f43ae1ae3b203ab01f2dba2e6c3a7
Surya T N <suryatej.nim@yahoo.com>
2022-10-27 16:44:46 +0000

    Updated CSS styling of JSX elements in Notification component and added code in AlertNotifications component to store all notifications data in an array of objects and passed it on as children prop to child instances of Notification component.

--- src/components/AlertNotifications/index.js
+++ src/components/AlertNotifications/index.js
@@ -6,28 +6,54 @@ import Notification from '../Notification'
 
 import './index.css'
 
-const notificationsData = [
-  <>
-    <AiFillCheckCircle className="success-notification" />
-  </>,
-  <>
-    <RiErrorWarningFill className="error-notification" />
-  </>,
-  <>
-    <MdWarning className="warning-notification" />
-  </>,
-  <>
-    <MdInfo className="info-notification" />
-  </>,
+const notificationData = [
+  {
+    icon: <AiFillCheckCircle className="success-notification" />,
+    heading: <h1 className="notification-header">Success</h1>,
+    description: (
+      <p className="notification-description">
+        You can access all the files in the folder
+      </p>
+    ),
+  },
+  {
+    icon: <RiErrorWarningFill className="error-notification" />,
+    heading: <h1 className="notification-header">Error</h1>,
+    description: (
+      <p className="notification-description">
+        Sorry, you are not authorized to have access to delete the file
+      </p>
+    ),
+  },
+  {
+    icon: <MdWarning className="warning-notification" />,
+    heading: <h1 className="notification-header">Warning</h1>,
+    description: (
+      <p className="notification-description">
+        Viewers of this file can see comments and suggestions
+      </p>
+    ),
+  },
+  {
+    icon: <MdInfo className="info-notification" />,
+    heading: <h1 className="notification-header">Info</h1>,
+    description: (
+      <p className="notification-description">
+        Anyone on the internet can view these files
+      </p>
+    ),
+  },
 ]
 
 const AlertNotifications = () => (
   <div className="alert-notifications-container">
     <h1 className="alert-notifications-header">Alert Notifications</h1>
     <ul className="alert-notifications-list">
-      {notificationsData.map(notificationsDataItem => (
+      {notificationData.map(notificationDataItem => (
         <Notification key={Math.floor(Math.random() * 1000)}>
-          {notificationsDataItem}
+          {notificationDataItem.icon}
+          {notificationDataItem.heading}
+          {notificationDataItem.description}
         </Notification>
       ))}
     </ul>
