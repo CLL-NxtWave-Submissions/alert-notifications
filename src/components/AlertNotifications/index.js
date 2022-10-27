9ff31f0ad836bd742e142750b1f19de0bfde9a75
Surya T N <suryatej.nim@yahoo.com>
2022-10-27 14:41:33 +0000

    Minimal code to render partial UI

--- src/components/AlertNotifications/index.js
+++ src/components/AlertNotifications/index.js
@@ -1,1 +1,43 @@
-// Write your code here
+import {AiFillCheckCircle} from 'react-icons/ai'
+import {RiErrorWarningFill} from 'react-icons/ri'
+import {MdWarning, MdInfo} from 'react-icons/md'
+
+import Notification from '../Notification'
+
+import './index.css'
+
+const notificationsData = {
+  data: (
+    <>
+      <AiFillCheckCircle className="success-notification" />
+    </>
+  ),
+  data: (
+    <>
+      <RiErrorWarningFill className="error-notification" />
+    </>
+  ),
+  data: (
+    <>
+      <MdWarning className="warning-notification" />
+    </>
+  ),
+  data: (
+    <>
+      <MdInfo className="info-notification" />
+    </>
+  ),
+}
+
+const AlertNotifications = () => (
+  <div className="alert-notifications-container">
+    <h1 className="alert-notifications-header">Alert Notifications</h1>
+    <ul className="alert-notifications-list">
+      {notificationsData.map(notificationsDataItem => (
+        <Notification></Notification>
+      ))}
+    </ul>
+  </div>
+)
+
+export default AlertNotifications
--- src/components/Notification/index.js
+++ src/components/Notification/index.js
@@ -1,1 +1,18 @@
-// Write your code here
+import {GrFormClose} from 'react-icons/gr'
+
+import './index.css'
+
+const Notification = props => {
+  const {children} = props
+
+  return (
+    <li className="notification-item">
+      <div className="notification-content-container">{children}</div>
+      <button type="button" className="notification-close-button">
+        <GrFormClose className="notification-close-button-icon" />
+      </button>
+    </li>
+  )
+}
+
+export default Notification
